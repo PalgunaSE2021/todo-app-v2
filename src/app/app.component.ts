da8c21014f4d39f66a8a5f0e9715fafa3e2bc839
Palguna M S <palgunamanchale@gmail.com>
2024-08-25 10:29:32 +0530

    added validations for input filed and fixed the delete button functionality inside task list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { DynamicDialogModule } from 'primeng/dynamicdialog';
 import { HomeComponent } from './home/home.component';
 import { CommonModule } from '@angular/common';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, CommonModule, DynamicDialogModule],
+  imports: [
+    RouterOutlet,
+    HomeComponent,
+    CommonModule,
+    DynamicDialogModule,
+    DragDropModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
